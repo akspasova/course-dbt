b0dbf0b703197009596b1b493ca4c869c3468939
Annie Spasova <s_spasova19@stud.hwr-berlin.de>
2023-10-02 06:40:26 +0000

    adjust attributes

--- greenery/models/curated_layer/cl_user_attributes.sql
+++ greenery/models/curated_layer/cl_user_attributes.sql
@@ -5,17 +5,37 @@ WITH user_data AS (
 ), address_data AS (
   SELECT *
   FROM {{ref('stg_postgres__addresses')}}
-) 
-SELECT user_id
-  , created_at_utc AS activated_at_utc
-  , first_name
-  , last_name
-  , email
-  , phone_number
-  , address
-  , zipcode
-  , state
-  , country
-  , IFF(row_num = 1, TRUE, FALSE) AS is_latest
+), orders_dataset AS (
+  SELECT *
+  FROM {{ref('stg_postgres__orders')}}
+), user_orders AS (
+  SELECT user_id
+    , COUNT(DISTINCT order_id) AS orders
+    , MAX(promo_id) AS promo_id
+  FROM orders_dataset
+  GROUP BY 1
+)
+SELECT u.user_id
+  , u.created_at_utc AS activated_at_utc
+  , u.first_name
+  , u.last_name
+  , u.email
+  , u.phone_number
+  , a.address
+  , a.zipcode
+  , a.state
+  , a.country
+  , IFF(u.row_num = 1, TRUE, FALSE) AS is_latest
+  , CASE
+      WHEN o.orders >= 6
+        THEN 'A'
+      WHEN o.orders BETWEEN 2 AND 5
+        THEN 'B'
+      WHEN o.orders = 1
+        THEN 'C'
+      ELSE 'D'
+    END AS vendor_grade
+  , IFF(o.promo_id IS NOT NULL, TRUE, FALSE) AS uses_promo_codes
 FROM user_data u
 LEFT JOIN address_data a ON u.address_id = a.address_id
+LEFT JOIN user_orders o ON u.user_id = o.user_id
